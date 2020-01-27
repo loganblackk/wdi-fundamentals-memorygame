5ec0d1a6189c2bc03530e53913abfaa633fb62d3
Loganblackk <Loganblack0@gmail.com>
2020-01-26 19:04:12 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,22 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cards = ["queen", "queen", "king", "king"];
 
+let cardsInPlay = [];
 
+let cardOne = cards[0];
+let cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) 
+  if(cardsInPlay[0] === cardsInPlay[1]){
+ alert("You found a match!");
+} else {
+    alert("Sorry, try again");
+}
+
+
+
+
